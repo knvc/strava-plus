5e18af50d74795649b436b99600fbee6fe3848db
knvc <koenvancrombrugge@outlook.com>
2021-01-09 16:39:55 +0100

    Setup project name and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import logo from "./logo/logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" width="100" height="100" />
         <h1>Strava Plus</h1>
       </header>
     </div>
