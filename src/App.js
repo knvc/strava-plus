8298983f685c07eb0e2cc9a9b43c2dd755c0fda6
knvc <koenvancrombrugge@outlook.com>
2021-01-10 16:43:55 +0100

    Add bootstrap css and login component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import logo from "./logo/logo.svg";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="Logo" width="100" height="100" />
         <h1>Strava Plus</h1>
+        <Login />
       </header>
     </div>
   );
