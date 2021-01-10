fc4e95836785021b56b2ebb6e4fbcd23089f2899
knvc <koenvancrombrugge@outlook.com>
2021-01-10 17:33:10 +0100

    Style login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import logo from "./logo/logo.svg";
 import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="Logo" width="100" height="100" />
-        <h1>Strava Plus</h1>
         <Login />
       </header>
     </div>
