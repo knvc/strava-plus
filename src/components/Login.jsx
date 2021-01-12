431b143b8a765ce0d67bd18810822c3369d3bbee
knvc <koenvancrombrugge@outlook.com>
2021-01-12 20:00:34 +0100

    Set html attributes to react specific attributes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,10 +8,10 @@ class Login extends Component {
             <React.Fragment>
              <form className="form-signin">
                  <img src={logo} alt="Logo" width="100" height="100" className="my-4" />
-                 <h2 class="h3 mb-3 font-weight-normal">Sign in</h2>
-                    <label for="inputClientId" className="sr-only">Client Id</label>
-                    <input type="text" id="inputClientId" className="form-control my-3" placeholder="Client Id" required autofocus />
-                    <label for="inputClientSecret" className="sr-only">Client Secret</label>
+                 <h2 className="h3 mb-3 font-weight-normal">Sign in</h2>
+                    <label htmlFor="inputClientId" className="sr-only">Client Id</label>
+                    <input type="text" id="inputClientId" className="form-control my-3" placeholder="Client Id" required autoFocus />
+                    <label htmlFor="inputClientSecret" className="sr-only">Client Secret</label>
                     <input type="text" id="inputClientSecret" className="form-control my-3" placeholder="Client Secret" required />
                     <button className="btn btn-lg btn-primary btn-block">Sign in</button>
                 </form>
